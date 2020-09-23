e4e14ce97b51ffd659c796046d1da4b49112dce4
Korbinian Habereder <haberederkorbinian@googlemail.com>
2020-09-23 12:22:40 +0200

    feature tile map :sparkles:
      - added tile map
    
    changelog-relevant

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace pactheman_client
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new PacTheManClient())
+                game.Run();
+        }
+    }
+}
